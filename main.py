8cfee2d6c38b5e38bf060f419c83d0b77cc35112
yenssyleon <ykleonm@correo.udistrital.edu.co>
2022-10-22 07:59:16 -0500

    CCOMMIT v.0.0.2

---
+++ main.py
@@ -0,0 +1,16 @@
+#Hola, este es un código, para hacer mi primer commit, qué emoción.
+#Se me ocurre hacer un convertidor de grados a radianes :)
+
+import math 
+
+print("main.pyESTA ES UNA CALCULADORA PARA CONVERTIR GRADOS A RADIANES :)")
+
+#Sí sirve:)
+
+print("Ingrese los grados:)")
+getgrades=int(input())
+
+class CalculadoraGR:
+    def Calculadora(self,getgrades):
+        rad=getgrades*(math.pi/180)
+        return rad
